ab3c9cd2b63b4e917f9134dc14723812b4441e0e
sweetymousewh <jack.w0529@gmail.com>
2017-04-10 13:27:02 +1000

    header created

---
+++ hello-world/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './component/H1';
+import { Alert } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <Header />
+        <p className="App-intro">
+          <Alert>To get started, edit <code>src/App.js</code> and save to reload.</Alert>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ hello-world/src/component/H1.js
@@ -0,0 +1,95 @@
+'use strict';
+
+import React from 'react';
+import { Row, Col} from 'reactstrap';
+import { Nav, NavbarToggler, Navbar, NavLink, Collapse, NavbarBrand } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+
+const rightColStyle = {
+    width : '100px'
+};
+
+const ToggleStyle = {
+    width : '100px',
+    margin : 'auto auto auto 20px',
+    backgroundColor : 'lightblue'
+};
+
+const centerRow = {
+    margin : 'auto'
+}
+ const BreadStyle = {
+    backgroundColor: 'transparent',
+    height : '40px'
+ }
+
+
+export default class Header extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+            collapsed: true
+        };
+    }
+
+    toggleNavbar() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
+    render(){
+        return (
+        <div>
+            <Navbar color="faded" light>
+                <Row>
+                    <Col xs="12" md="8">
+                        <Row>
+                            <Col md="2">
+                                <NavbarToggler style={ToggleStyle} onClick={this.toggleNavbar} />
+                            </Col>
+                            <Col md="2">
+                                <Breadcrumb style={BreadStyle}>
+                                    <BreadcrumbItem>HomePage</BreadcrumbItem>
+                                </Breadcrumb>
+
+                            </Col>
+                        </Row>
+                        <Row style={centerRow}>
+                            <Collapse className="navbar-toggleable-md" isOpen={!this.state.collapsed}>
+                                        <NavbarBrand>
+                                            <Nav>
+                                                <NavLink href="#">Homepage</NavLink>
+                                                <NavLink href="#">List</NavLink>
+                                                <NavLink href="#">Records</NavLink>
+                                                <NavLink href="#">Contact us</NavLink>
+                                            </Nav>
+                                        </NavbarBrand>
+                            </Collapse>
+                        </Row>
+                    </Col>
+                    <Col xs="6" md="4">
+                        <Nav navbar>
+                            <Row>
+                                <Col/>
+                                <Col>
+                                    <NavLink href="/components/">Components</NavLink>
+                                </Col>
+                                <Col>
+                                    <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+                                </Col>
+                            </Row>
+                        </Nav>
+                    </Col>
+                </Row>
+            </Navbar>
+
+
+
+        </div>
+
+        );
+    }
+}
